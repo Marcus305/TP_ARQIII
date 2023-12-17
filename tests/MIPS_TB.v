85067918138aff8e0978388ae0beb1ffbbbf5037
Marcus305 <marcusleandro12357@gmail.com>
2023-12-16 23:39:33 -0300

    update test bench

--- tests/MIPS_TB.v
+++ tests/MIPS_TB.v
@@ -1,18 +1,24 @@
 `timescale 1 ps / 100 fs
+
 module MIPS_TB();
-parameter ClockDelay = 5000;
+  parameter ClockDelay = 5000;
 
-reg clk,reset;
+  reg clk, reset;
+  mips myMIPS(clk, reset);
 
+  initial begin
+    clk = 0;
+    reset = 1;
+    #(ClockDelay/4);
+    reset = 0;
+  end
 
-mips myMIPS(clk, reset);
-initial clk = 0;
-always #(ClockDelay/2) clk = ~clk;
+  always # (ClockDelay/2) clk = ~clk;
 
-initial 
-begin
-   reset = 1;
-  #(ClockDelay/4);
-  reset = 0;
-end
+  always @(posedge clk) begin
+    if (myMIPS.Instruction) begin
+      $display("PC: %b | Instruction: %b | ReadData1: %b | ReadData2: %b | WB_WriteData: %b | Bus_A_ALU: %b | Bus_B_ALU: %b", 
+               myMIPS.PC, myMIPS.Instruction, myMIPS.ReadData1, myMIPS.ReadData2, myMIPS.WB_WriteData, myMIPS.Bus_A_ALU, myMIPS.Bus_B_ALU);
+    end
+  end
 endmodule
